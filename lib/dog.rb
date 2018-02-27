7c31872dd4c000664d86d306ddb1ac505915f3d3
YanC Lin <yonalin1@gmail.com>
2018-02-27 17:25:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,11 @@ class Dog
   attr_accessor :name, :breed
    attr_reader :id
 
-  def initialize(name,breed,id=nil,key:,value:)
+  def initialize(name,breed,id=nil,key,value)
     @name = name
     @breed = breed
     @id = id
-    @key = key
-    @value = value
+    (name,breed,id).each do |key,value|
   end
 
 end
